32369b5c15fb6df04530bd6fd9a1aa8592b5fa56
Volkan Eligül <volkan.eligul@hepsiburada.com>
2015-12-31 14:14:12 +0200

    Polymer first check-in

---
+++ Polymer/Polymer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Polymer.Startup))]
+namespace Polymer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
